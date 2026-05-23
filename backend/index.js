053a11dfa596b036f7772ee5902f7faee505faa8
Barath2456 <barathbravo2005@gmail.com>
2026-05-23 23:28:55 +0530

    fix cors

--- backend/index.js
+++ backend/index.js
@@ -16,7 +16,10 @@ const PORT = process.env.PORT || 5000;
 // Enable CORS for Next.js Frontend
 app.use(
   cors({
-    origin: process.env.FRONTEND_URL || "http://localhost:3000",
+    origin: [
+      "http://localhost:3000",
+      "https://college-hub-three.vercel.app",
+    ],
     credentials: true,
   })
 );
